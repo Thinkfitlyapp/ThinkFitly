04aec3ebed535a9af1476a7e8d6ac3ddfb0c7ad2
jonathan Santos <jons@thinkfitly.com>
2013-12-17 14:15:34 -0500

    Initial Git Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thinkfitly::Application.config.secret_key_base = '21eb3a4b37984fd7da09350e223e58ff03f70363e8f636bf4c1b7c1d8a3553d7fd9077ddd9dc6bad8f756ae946e7168bf60c738689d8b80989f7e791b026d965'
