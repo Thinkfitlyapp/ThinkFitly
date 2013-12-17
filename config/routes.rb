3565ee652ce5be35bb82f3b2fcee8181a50451d4
jonathan Santos <jons@thinkfitly.com>
2013-12-17 15:14:15 -0500

    Added pages Home, About, Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Thinkfitly::Application.routes.draw do
   root "pages#home"
+  
+  get "signup" => "pages#signup"
+  get "about" => "pages#about"
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
