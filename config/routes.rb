f33ae8ee710a36da26663f352a236ec2d97d9880
jonathan Santos <jons@thinkfitly.com>
2013-12-17 18:10:01 -0500

    NavBar font size/family customization

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Thinkfitly::Application.routes.draw do
   
   get "signup" => "pages#signup"
   get "about" => "pages#about"
-  
+  get "login" => "pages#login"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
